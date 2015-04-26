915a73b05ce09544df5c13f48453015cc19222a2
theogirl <theogirl@outlook.com>
2015-04-26 17:09:28 -0400

    initial commit-tests

---
+++ js/test-js.js
@@ -0,0 +1,13 @@
+$(document).ready (function() {
+	$('.append').children().append("... has been appended!");
+	$('.prepend').children().prepend("I am prepending ");
+
+	$('input')
+		.keydown(function() {
+		if (event.which == 13 )//if the user presses the Enter key after they input text in the field
+		var value = $( this ).val();//(declares a variable called "value" which is defined as the current value of the "this" element, which in this case is the "input" element)
+    	$('.output').text( value );//then display the text (now known as the variable defined above as "value") in the selected DOM element, which is the div with class = "output"
+    	$('delete-btn').css('display', 'inline');
+		});
+
+});
